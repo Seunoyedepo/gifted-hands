6ce7c50f15f0ea313bb2797f8e4022309fbb8365
Oluwaseun Olumide Oyedepo <oluwaseunolumideoyedepo@gmail.com>
2026-07-03 20:04:23 -0700

    Begin Javascript fundamentals

---
+++ js/newscript.js
@@ -0,0 +1,9 @@
+let organization = "Gifted Hands";
+let city = "Ibadan";
+let founder = "Seun Oyedepo";
+let volunteer = 120;
+
+console.log(organization);
+console.log(city);
+console.log(founder);
+console.log(volunteer);
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// My first Javascript program
+console.log("Welcome to Gifted Hands");
+
+// Learning Javascript
+console.log("Day 11");
+
+// My first variable
+let charity = "Green Pastures";
+console.log(charity);
