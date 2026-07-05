835bd493e1647a40992b9f29b4263dcd19151df3
Oluwaseun Olumide Oyedepo <oluwaseunolumideoyedepo@gmail.com>
2026-07-05 14:13:23 -0700

    Learn Javascript variables and data types

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
-// My first Javascript program
-console.log("Welcome to Gifted Hands");
 
-// Learning Javascript
-console.log("Day 11");
 
-// My first variable
-let charity = "Green Pastures";
-console.log(charity);
+let organization = "Gifted Hands";
+let city = "Ibadan";
+let volunteers = 50;
+let acceptingDonations = true;
+
+console.log(typeof organization);
+console.log(typeof city);
+console.log(typeof volunteers);
+console.log(typeof acceptingDonations);
+
+volunteers = 120;
+console.log(volunteers);
+
+city = "Lagos";
+console.log(city);
+
+
