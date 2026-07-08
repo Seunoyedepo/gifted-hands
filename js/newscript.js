bf411078d698093436bea6c64aec9969232321e6
Oluwaseun Olumide Oyedepo <oluwaseunolumideoyedepo@gmail.com>
2026-07-07 22:20:33 -0700

    Learn data operators and logical operators

--- js/newscript.js
+++ js/newscript.js
@@ -1,9 +1,78 @@
-let organization = "Gifted Hands";
-let city = "Ibadan";
-let founder = "Seun Oyedepo";
-let volunteer = 120;
+ organization = "Gifted Hands";
+ city = "Ibadan";
+ founder = "Seun Oyedepo";
+ volunteer = 120;
 
 console.log(organization);
 console.log(city);
 console.log(founder);
 console.log(volunteer);
+
+age = 17;
+if(age >=18){
+    console.log("You are eligible to volunteer");
+}else{
+    console.log("You are not eligible to volunteer");
+}
+
+donation = 4000;
+if(donation >= 5000){
+    console.log("Thanks for your generous support");
+} else{
+    console.log("Thanks for supporting Gifted Hands");
+}
+
+let score = 59;
+if(score >= 60){
+    console.log("Pass");
+}else{
+    console.log("Fail");
+}
+
+donation = 7000;
+
+if (donation >= 7000){
+    console.log ("Platinum Donor!");
+} else if (donation >= 5000){
+    console.log("Gold Donor!");
+} else {
+    console.log("Thank you for supporting Gifted Hands");
+}
+
+score = 49;
+
+if (score >=70) {
+    console.log("A");
+} else if (score >= 60){
+    console.log("B");
+} else if (score >= 50){
+    console.log("C");
+} else {
+    console.log("Fail");
+}
+
+age = 17;
+let completedForm = false;
+
+if (age >=18 && completedForm){
+    console.log("Eligible to volunteer");
+} else{
+    console.log("Not eligible to volunteer");
+}
+
+let firstAid = true;
+let teaching = false;
+
+if (firstAid || teaching){
+    console.log("Volunteer Accepted");
+} else{
+    console.log("Volunteer Rejected");
+}
+
+let registrationClosed = true;
+
+if (!registrationClosed){
+    console.log("Registration is Open");
+} else {
+    console.log("Registration is Closed")
+}
--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 
-
 let organization = "Gifted Hands";
 let city = "Ibadan";
 let volunteers = 50;
@@ -16,4 +15,40 @@ console.log(volunteers);
 city = "Lagos";
 console.log(city);
 
+let donation1 = 5000;
+let donation2 = 3000;
 
+console.log(donation1 + donation2);
+console.log(donation1 - donation2);
+console.log(donation1 * donation2);
+console.log(donation1 / donation2);
+
+console.log(10 % 3);
+console.log(20 % 5);
+
+let firstName = "Seun";
+let lastName = "Oyedepo";
+console.log(firstName + " - " + lastName);
+
+ organization = "Gifted Hands";
+ city = "Ibadan";
+console.log(organization + " is located in " + city);
+
+console.log(20 > 18);
+console.log(15 > 18);
+console.log(18 >= 18);
+console.log(16 < 18);
+console.log(10 === 10);
+console.log(10 === "10");
+
+let age = 20;
+if (age >= 18){
+    console.log("You are eligible to register");
+}
+
+let donation = 3000;
+if (donation >= 5000){
+    console.log("Thank you for your generous donation!");
+} else{
+    console.log("Thank you for supporting Gifted Hands");
+}
